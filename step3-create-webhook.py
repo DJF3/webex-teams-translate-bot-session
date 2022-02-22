7b2c35775117141655965fd464882dd3efc490b4
DJ <github@nldj.com>
2022-02-22 14:50:57 +0100

    Update step3-create-webhook.py

--- step3-create-webhook.py
+++ step3-create-webhook.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 # by DJ Uittenbogaard - duittenb@cisco.com
-# Code is part of a session for partners "create a Webex Teams bot in Python"
+# Code is part of a session for partners "create a Webex bot in Python"
 from webexteamssdk import WebexTeamsAPI
 import os
 
+# put your bot token in an environment variable 'PIW_BOT_TOKEN' 
 BOT_TOKEN = os.environ.get('PIW_BOT_TOKEN')
 # OR --> BOT_TOKEN = "put_bot_token_here"
 api = WebexTeamsAPI(access_token=BOT_TOKEN)
 
-api.webhooks.create("Partner Bot Demo webhook 2020", "bot_webhook_url_here", "messages", "created")
+api.webhooks.create("Partner Bot Demo webhook", "BOT_WEBHOOK_URL", "messages", "created")
+
+# Run this script to create the Bot Webhook that notifies the "BOT_WEBHOOK_URL" when a new "message" is "created"
