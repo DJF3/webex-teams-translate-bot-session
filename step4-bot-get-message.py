1da962e5c15653c0ef5f7d29a08bbb4bf0ba4ab9
DJ <github@nldj.com>
2022-02-22 14:52:46 +0100

    Update step4-bot-get-message.py

--- step4-bot-get-message.py
+++ step4-bot-get-message.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 # by DJ Uittenbogaard - duittenb@cisco.com
-# Code is part of a session for partners "create a Webex Teams bot in Python"
+# Code is part of a session for partners "create a Webex bot in Python"
 from flask import Flask, jsonify, request
 from webexteamssdk import WebexTeamsAPI
 import os
 
-# below: or BOT_TOKEN = "put_bot_token_here"
+# put your bot token in an environment variable 'PIW_BOT_TOKEN' OR replace below with: BOT_TOKEN = "put_bot_token_here"
 BOT_TOKEN = os.environ.get('PIW_BOT_TOKEN')
 api = WebexTeamsAPI(access_token=BOT_TOKEN)
 app = Flask(__name__)
@@ -23,3 +23,5 @@ def webhook():
     return jsonify({"success": True})
 
 app.run(host='0.0.0.0', port=2099, debug=True)
+
+# Run this code and send a message to the Bot. The message should appear in the window running your code.
