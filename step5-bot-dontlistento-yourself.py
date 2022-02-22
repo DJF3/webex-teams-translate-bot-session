cb4d93a302ca903f6eb388613f8a0f3f7d1c30c7
DJ <github@nldj.com>
2022-02-22 14:54:48 +0100

    Update step5-bot-dontlistento-yourself.py

--- step5-bot-dontlistento-yourself.py
+++ step5-bot-dontlistento-yourself.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 # by DJ Uittenbogaard - duittenb@cisco.com
-# Code is part of a session for partners "create a Webex Teams bot in Python"
+# Code is part of a session for partners "create a Webex bot in Python"
 from flask import Flask, jsonify, request
 from webexteamssdk import WebexTeamsAPI
 import os
 
-# below: or BOT_TOKEN = "put_bot_token_here"
+# put your bot token in an environment variable 'PIW_BOT_TOKEN' OR replace below with: BOT_TOKEN = "put_bot_token_here"
 BOT_TOKEN = os.environ.get('PIW_BOT_TOKEN')
 api = WebexTeamsAPI(access_token=BOT_TOKEN)
 app = Flask(__name__)
@@ -20,13 +20,15 @@ def webhook():
     # extract message text from the message
     my_message = message.text
     # do not respond to messages from the bot itself
-    if message.personEmail == "your_bot_email@webex.bot":
-        print("**** NOT RESPONDING TO OWN MESSAGE!! ****")
+    if "@webex.bot" in message.personEmail:
+        print("** NOT RESPONDING TO OWN MESSAGE!! **")
         pass
     else:
-        print("the message TEXT is: " + str(my_message))
+        print(f"the message TEXT is: {my_message}")
         api.messages.create(toPersonId=message.personId, markdown="welcome to this session")
 
     return jsonify({"success": True})
 
 app.run(host='0.0.0.0', port=2099, debug=True)
+
+# Run this code and send a message to the bot. It will respond with "welcome to this session"
