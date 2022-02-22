59f971e0732c2297d6e4e1676e9047f991804d4c
DJ <github@nldj.com>
2022-02-22 14:44:06 +0100

    Update step1-webserver.py

--- step1-webserver.py
+++ step1-webserver.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # by DJ Uittenbogaard - duittenb@cisco.com
-# Code is part of a session for partners "create a Webex Teams bot in Python"
+# Code is part of a session for partners "create a Webex bot in Python"
 from flask import Flask
 
 app = Flask(__name__)
@@ -10,3 +10,5 @@ def index():
     return "Hello PIW attendees"
 
 app.run(port=2099)
+
+# run this python code and browse to 'http://localhost:2099'
